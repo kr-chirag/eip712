ce899707d9bd5068408a005237dac796d4bd3175
kr-chirag <chirag.p@knackroot.com>
2025-04-18 10:09:15 +0530

    fix: constructor args

--- deploy/EIP712.ts
+++ deploy/EIP712.ts
@@ -4,7 +4,7 @@ export default async (hre: HardhatRuntimeEnvironment) => {
     const { deployer } = await hre.getNamedAccounts();
     const demo = await hre.deployments.deploy("EIP712", {
         from: deployer,
-        args: [deployer, 11155111]
+        args: [11155111, "1"],
     });
     console.log("EIP712 Deployed at:", demo.address, demo.newlyDeployed);
 };
--- ethers/EIP712.ts
+++ ethers/EIP712.ts
@@ -8,7 +8,6 @@ const RPC = `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`;
 const PRIVATE_KEY = `${process.env.DEPLOYER_KEY}`;
 
 async function main() {
-
     const provider = new ethers.JsonRpcProvider(RPC);
     const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 
@@ -16,24 +15,26 @@ async function main() {
         name: "EIP712",
         version: "1",
         chainId: 11155111,
-        verifyingContract: "0x5e17b14ADd6c386305A32928F985b29bbA34Eff5",
+        verifyingContract: "0x81dF732e10acEE4c195ee2467Fe4A4423a15C993",
     };
 
     const types = {
         Account: [
-        { name: "name", type: "string" },
+            { name: "name", type: "string" },
             { name: "amount", type: "uint256" },
         ],
     };
 
-    const signature = await signer.signTypedData(domain, types, {name: "csp", amount:1249});
-    const {v,r,s} = ethers.Signature.from(signature);
+    console.log("domail:", TypedDataEncoder.hashDomain(domain));
+    console.log("hashStruct:", TypedDataEncoder.hashStruct("Account", types, { name: "csp", amount: 1249 }));
+
+    const signature = await signer.signTypedData(domain, types, { name: "csp", amount: 1249 });
+    const { v, r, s } = ethers.Signature.from(signature);
 
     console.log("r:", r);
     console.log("s:", s);
     console.log("v:", v);
     console.log("address:", signer.address);
-
 }
 
 main().catch(console.log);
